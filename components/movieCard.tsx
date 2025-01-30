4bb74e9673ae0e98f973e6b018b382427832b2ca
Larusso254 <imeldalarusso@gmail.com>
2025-01-31 01:53:23 +0300

    style: fixes responsiveness on top 10 movies. Signed-off-by: Imelda Hope <imelda.hope@ihope.dev>

--- components/movieCard.tsx
+++ components/movieCard.tsx
@@ -10,12 +10,11 @@ const MovieCard = ({ poster_path, movie_id }: { poster_path: string, movie_id:nu
    };
   
   return (
-    <div className="" onClick={() => handleClick(movie_id)}>
+    <div className="w-20 h-32 lg:h-60 lg:w-40 m-5" onClick={() => handleClick(movie_id)}>
       <Image
         src={`https://image.tmdb.org/t/p/w500${poster_path}`}
         alt="some show"
-        width={160}
-        height={240}
+        fill
         className="rounded-md cursor-pointer"
       />
     </div>
--- components/topMovies.tsx
+++ components/topMovies.tsx
@@ -7,17 +7,17 @@ const TopMovies = () => {
   const { data, isLoading, error } = usePopularMovies();
 
   return (
-    <div className="p-5">
+    <div className="m-8">
       <h1 className="mb-2 text-xl font-black lg:text-2xl">
         Top 10 <span className="text-md font-light">in Movies</span> this Week
       </h1>
-      <div className="flex gap-7">
+      <div className="flex gap-16 overflow-x-scroll">
         {data?.results.slice(0, 10).map((movie, index) => (
-          <div key={movie.id} className="relative flex items-center p-2">
+          <div key={movie.id} className="relative flex items-center p-2 m-3 ">
             <div className="z-10">
               <MovieCard poster_path={movie.poster_path} movie_id ={movie.id} />
             </div>
-            <h1 className="absolute -left-10 top-1/2 -translate-y-1/2 text-11xl">
+            <h1 className="absolute -left-16 top-1/2 -translate-y-1/2 text-11xl">
               {index + 1}
             </h1>
           </div>

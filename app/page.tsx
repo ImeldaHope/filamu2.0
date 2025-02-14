e171af0bb282851a4c4b8e3015b7c515589b9a11
Larusso254 <imeldalarusso@gmail.com>
2025-02-14 13:06:16 +0300

    refactor: updates components to reuse genre card  Signed-off-by: Imelda Hope <imelda.hope@ihope.dev>

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import GenreCard from "@/components/genreCard";
-import GenreCardSeries from "@/components/genreCardSeries";
 import Hero from "@/components/hero";
 import NewRelease from "@/components/newRelease";
 import NewReleaseSeries from "@/components/newReleaseSeries";
@@ -10,20 +9,16 @@ import UpcomingSeries from "@/components/upcomingSeries";
 
 export default function Home() {
   return (
-    
-      <>
+    <>
       {/* <Hero />       */}
-      <GenreCard />
+      <GenreCard type="movie" />
       <NewRelease />
-      <TopMovies />      
+      <TopMovies />
       <UpcomingMovies />
-      <GenreCardSeries />  
+      <GenreCard type="series" />
       <NewReleaseSeries />
       <TopSeries />
       <UpcomingSeries />
-      
-      
-      </>
-    
+    </>
   );
 }
--- components/genreCard.tsx
+++ components/genreCard.tsx
@@ -1,20 +1,26 @@
 "use client";
 import React from "react";
 import GenreBtn from "./genreBtn";
-import { useMovieGenre } from "@/hooks";
+import { useMovieGenre, useSeriesGenre } from "@/hooks";
 import style from "../app/custom.module.css";
-const GenreCard = () => {
-  const { data, isLoading, error } = useMovieGenre();
+const GenreCard = ({ type }: { type: "movie" | "series" }) => {
+  const { data, isLoading, error } =
+    type === "movie" ? useMovieGenre() : useSeriesGenre();
 
   return (
     <div className="m-8">
       <h1 className="mb-2 text-xl font-black lg:text-2xl">
-        Popular Genres <span className="text-md font-light">in Movies</span>
+        Popular Genres{" "}
+        <span className="text-md font-light">
+          in {type === "movie" ? "Movies" : "TV Shows"}
+        </span>
       </h1>
       <div className={`flex gap-5 overflow-x-scroll ${style.scrollbar_hide}`}>
         {data?.genres
           .slice(0, 8)
-          .map((genre) => <GenreBtn genre={genre} key={genre.id} />)}
+          .map((genre) => (
+            <GenreBtn genre={genre} key={genre.id} type={type} />
+          ))}
       </div>
     </div>
   );
